e4db875370f833db51c362a6775293fd21557bf7
Leander <leander.duft@kfnmail.ch>
2016-05-24 16:27:38 +0200

    added search function

--- ÖvInfo/Form1.Designer.cs
+++ ÖvInfo/Form1.Designer.cs
@@ -28,34 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnHi = new System.Windows.Forms.Button();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.lblFrom = new System.Windows.Forms.Label();
+            this.selectionFrom = new System.Windows.Forms.ComboBox();
+            this.selectionTo = new System.Windows.Forms.ComboBox();
+            this.lblTo = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnHi
+            // btnsearch
             // 
-            this.btnHi.Location = new System.Drawing.Point(12, 12);
-            this.btnHi.Name = "btnHi";
-            this.btnHi.Size = new System.Drawing.Size(268, 249);
-            this.btnHi.TabIndex = 0;
-            this.btnHi.Text = "Hi There!";
-            this.btnHi.UseVisualStyleBackColor = true;
-            this.btnHi.Click += new System.EventHandler(this.btnHi_Click);
+            this.btnsearch.Location = new System.Drawing.Point(12, 155);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(268, 39);
+            this.btnsearch.TabIndex = 0;
+            this.btnsearch.Text = "Search Connection";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            this.btnsearch.Click += new System.EventHandler(this.btnsearch_Click);
+            // 
+            // lblFrom
+            // 
+            this.lblFrom.AutoSize = true;
+            this.lblFrom.Location = new System.Drawing.Point(12, 15);
+            this.lblFrom.Name = "lblFrom";
+            this.lblFrom.Size = new System.Drawing.Size(33, 13);
+            this.lblFrom.TabIndex = 3;
+            this.lblFrom.Text = "From:";
+            // 
+            // selectionFrom
+            // 
+            this.selectionFrom.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.selectionFrom.FormattingEnabled = true;
+            this.selectionFrom.Location = new System.Drawing.Point(89, 12);
+            this.selectionFrom.Name = "selectionFrom";
+            this.selectionFrom.Size = new System.Drawing.Size(191, 21);
+            this.selectionFrom.TabIndex = 5;
+            this.selectionFrom.UseWaitCursor = true;
+            this.selectionFrom.TextChanged += new System.EventHandler(this.selectionFrom_TextChanged);
+            this.selectionFrom.MouseClick += new System.Windows.Forms.MouseEventHandler(this.selectionFrom_MouseClick);
+            // 
+            // selectionTo
+            // 
+            this.selectionTo.FormattingEnabled = true;
+            this.selectionTo.Location = new System.Drawing.Point(89, 50);
+            this.selectionTo.Name = "selectionTo";
+            this.selectionTo.Size = new System.Drawing.Size(191, 21);
+            this.selectionTo.TabIndex = 6;
+            this.selectionTo.UseWaitCursor = true;
+            this.selectionTo.TextChanged += new System.EventHandler(this.selectionTo_TextChanged);
+            this.selectionTo.MouseClick += new System.Windows.Forms.MouseEventHandler(this.selectionTo_MouseClick);
+            // 
+            // lblTo
+            // 
+            this.lblTo.AutoSize = true;
+            this.lblTo.Location = new System.Drawing.Point(12, 58);
+            this.lblTo.Name = "lblTo";
+            this.lblTo.Size = new System.Drawing.Size(23, 13);
+            this.lblTo.TabIndex = 7;
+            this.lblTo.Text = "To:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
-            this.Controls.Add(this.btnHi);
+            this.ClientSize = new System.Drawing.Size(292, 211);
+            this.Controls.Add(this.lblTo);
+            this.Controls.Add(this.selectionTo);
+            this.Controls.Add(this.selectionFrom);
+            this.Controls.Add(this.lblFrom);
+            this.Controls.Add(this.btnsearch);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnHi;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.Label lblFrom;
+        private System.Windows.Forms.ComboBox selectionFrom;
+        private System.Windows.Forms.ComboBox selectionTo;
+        private System.Windows.Forms.Label lblTo;
     }
 }
 
--- ÖvInfo/Form1.cs
+++ ÖvInfo/Form1.cs
@@ -19,11 +19,57 @@ namespace ÖvInfo
             InitializeComponent();
         }
 
-        private void btnHi_Click(object sender, EventArgs e)
+        private void btnsearch_Click(object sender, EventArgs e)
         {
+
+
+        }
+
+        private void txtOrt_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void selectionFrom_TextChanged(object sender, EventArgs e)
+        {
+            
+            selectionFrom.Items.Clear();
+
+           selectionFrom.Select(selectionFrom.Text.Length, selectionFrom.Text.Length);
+
+            Stations From = new Stations();
             Transport transport = new Transport();
 
-            MessageBox.Show(transport.ToString());
+            From = transport.GetStations(selectionFrom.Text);
+
+            foreach (Station Hold in From.StationList)
+                selectionFrom.Items.Add(Hold.Name);
+        }
+
+        private void selectionFrom_MouseClick(object sender, MouseEventArgs e)
+        {
+            selectionFrom.DroppedDown = true;
+        }
+
+        private void selectionTo_MouseClick(object sender, MouseEventArgs e)
+        {
+            selectionTo.DroppedDown = true;
+        }
+
+        private void selectionTo_TextChanged(object sender, EventArgs e)
+        {
+            selectionTo.Items.Clear();
+
+            selectionTo.Select(selectionTo.Text.Length, selectionTo.Text.Length);
+
+            Stations To = new Stations();
+            Transport transport = new Transport();
+
+            To = transport.GetStations(selectionFrom.Text);
+
+            foreach (Station Hold in To.StationList)
+                selectionTo.Items.Add(Hold.Name);
         }
     }
 }
